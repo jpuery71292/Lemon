3b4127a83011e4a5120c5e371b2789fc42df761f
jpuery71292 <jpuery@gmail.com>
2021-12-10 12:08:01 -0300

    responsive

--- src/components/cuerpo/navbar.jsx
+++ src/components/cuerpo/navbar.jsx
@@ -1,12 +1,14 @@
 import {
   Container, 
   Nav, 
+  NavDropdown
 } 
 from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from '../imagenes/logo.png'
+import { FiMenu } from 'react-icons/fi'
 
 function Menu() {
   return(
@@ -22,6 +24,15 @@ function Menu() {
             <Nav.Link href="/redes">Redes</Nav.Link>
           </div>
         </div>
+          <div className="Menu-responsive">
+            <Nav>
+              <NavDropdown title={<FiMenu />} id="basic-nav-dropdown">
+                <NavDropdown.Item href="/nosotros">Nosotros</NavDropdown.Item>
+                <NavDropdown.Item href="/master&contacto">Mezcla</NavDropdown.Item>
+                <NavDropdown.Item href="/redes">Redes</NavDropdown.Item>
+              </NavDropdown>
+            </ Nav>
+          </div>
       </Container>
     </div>
   )
